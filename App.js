7f7e50849a9298b95ce125f90ef7d987a91bdbca
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2022-06-09 23:30:49 -0300

    🚧 create idea of refresh animation

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-import MainScreen from "./app/screens/MainScreen";
+import React from 'react'
+import MainScreen from "./app/screens/MainScreen"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
-export default MainScreen
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <MainScreen />
+    </GestureHandlerRootView>
+  )
+}
--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, SafeAreaView, TextInput, ActivityIndicator, PermissionsAndroid } from 'react-native'
+import { PanGestureHandler } from 'react-native-gesture-handler'
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withSpring, runOnJS } from 'react-native-reanimated'
+
 import Geolocation from 'react-native-geolocation-service'
+
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import useWeather from '../api/useWeather'
 import useLocation from '../api/useLocation'
@@ -12,26 +17,24 @@ import Icon from 'react-native-vector-icons/Ionicons'
 const MainScreen = () => {
   const [weather, setWeather] = useState()
   const [location, setLocation] = useState()
+  const [isLoading, setIsLoading] = useState(true)
 
   const weatherApi = useWeather()
   const locationApi = useLocation()
 
+  const verticalPosition = useSharedValue(0)
+  const loadRotation = useSharedValue(0)
+
+  const AnimatedIcon = Animated.createAnimatedComponent(MaterialIcon)
+
   useEffect(() => {
     const requestPermisionAndLocation = async () => {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        {
-          'title': 'Test',
-          'message': 'test'
-        }
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
       )
-
-      console.log(granted)
 
       if (granted == 'granted') {
         Geolocation.getCurrentPosition(async position => {
-          console.log(position)
-
           const weatherInfo = await weatherApi.get('onecall', {
             params: {
               lat: position.coords.latitude,
@@ -48,12 +51,20 @@ const MainScreen = () => {
     
           setWeather(weatherInfo.data)
           setLocation(locationInfo.data)
+
+          closeRefresh()
+
+          setTimeout(() => {
+            setIsLoading(false)
+          }, 1000)
         })
       }
     }
 
-    requestPermisionAndLocation()
-  }, [])
+    if (isLoading) {
+      requestPermisionAndLocation()
+    }
+  }, [isLoading])
 
   const renderBasicWeatherInfo = () => {
     if (weather && location) {
@@ -67,16 +78,63 @@ const MainScreen = () => {
     )
   }
 
+  const renderRefreshAnimation = () => {
+    if (isLoading) {
+      return <ActivityIndicator color='#9A9A9A' style={{ position: 'absolute', alignSelf: 'center', top: 30 }} />
+    }
+
+    return <AnimatedIcon color='#9A9A9A' style={[{ position: 'absolute', alignSelf: 'center', top: 30 }, rotateAnimation]} size={20} name='loading' />
+  }
+
+  const panGestureEvent = useAnimatedGestureHandler({
+    onStart: (event) => {},
+    onActive: (event) => {
+      if (event.velocityY >= 0 && verticalPosition.value <= 200) {
+        verticalPosition.value += 0.01 * event.velocityY
+        loadRotation.value += 6
+      }
+    },
+    onEnd: (event) => {
+      if (event.y >= 90) {
+        verticalPosition.value = withSpring(100)
+        runOnJS(setIsLoading)(true)
+      } else {
+        verticalPosition.value = withSpring(0)
+      }
+    }
+  })
+
+  const closeRefresh = () => {
+    verticalPosition.value = withSpring(0)
+  }
+
+  const refreshAnimation = useAnimatedStyle(() => {
+    return {
+      top: verticalPosition.value
+    }
+  })
+
+  const rotateAnimation = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotateZ: `${loadRotation.value}deg` }]
+    }
+  })
+
   return (
-    <View style={styles.wrapper}>
-      <SafeAreaView style={styles.mainContainer}>
-        <View>
-          <Icon size={20} name='search-sharp' style={styles.searchIcon} />
-          <TextInput placeholderTextColor='#C4C4C4' placeholder='Procure uma localização' style={styles.searchInput} />
-        </View>
-        {renderBasicWeatherInfo()}
-      </SafeAreaView>
-    </View>
+    <>
+      {renderRefreshAnimation()}
+      <PanGestureHandler onGestureEvent={panGestureEvent}>
+        <Animated.View style={[styles.wrapper, refreshAnimation]}>
+          <SafeAreaView style={styles.mainContainer}>
+            <View>
+              <Icon size={20} name='search-sharp' style={styles.searchIcon} />
+              <TextInput placeholderTextColor='#C4C4C4' placeholder='Procure uma localização' style={styles.searchInput} />
+            </View>
+            {renderBasicWeatherInfo()}
+          </SafeAreaView>
+        </Animated.View>
+      </PanGestureHandler>
+    </>
   )
 }
 
